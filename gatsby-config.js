6bddf07ca7a2b32830e42b00989cad8d069e9fc7
jongwooo <han980817@gmail.com>
2020-01-31 00:11:16 +0900

    feat: Update siteMetadata in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Starter Typescript`,
+    description: `Typescript version of the default Gatsby starter`,
+    author: `@jongwooo`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

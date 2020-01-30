6b469045c334b0786a95bee2cf9483c139084efa
jongwooo <han980817@gmail.com>
2020-01-31 02:55:29 +0900

    feat: Change layout.js to layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,14 +5,14 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
+import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
 
-const Layout = ({ children }) => {
+const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -22,7 +22,7 @@ const Layout = ({ children }) => {
       }
     }
   `)
-
+  
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />

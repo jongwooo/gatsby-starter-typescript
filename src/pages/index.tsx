689648f8932b6d5bc97b393a91f80696b9532c9b
jongwooo <han980817@gmail.com>
2020-03-07 03:55:34 +0900

    feat: Update index.tsx to change title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ import SEO from "../components/seo";
 const IndexPage: React.FC = () => (
 	<Layout>
 		<SEO title="Home" />
-		<h1>Hi people and Typescript!</h1>
+		<h1>Hi people and TypeScript!</h1>
 		<p>Welcome to your new Gatsby site.</p>
 		<p>Now go build something great.</p>
 		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>

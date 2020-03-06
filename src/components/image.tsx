bac172f5ede1d383e744affc30e87fad931cbe8a
jongwooo <han980817@gmail.com>
2020-03-07 03:45:28 +0900

    style: Update components to format code

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,6 +1,7 @@
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+/* eslint-disable no-unused-vars */
+import * as React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -14,19 +15,19 @@ import Img from "gatsby-image"
  */
 
 const Image: React.FC = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+	const data = useStaticQuery(graphql`
+		query {
+			placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 300) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`);
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
-}
+	return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
+};
 
-export default Image
+export default Image;

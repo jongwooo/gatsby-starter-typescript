c3bb6e47cfd2617d0c3624c2558d21ff79098228
jongwooo <han980817@gmail.com>
2022-08-13 19:20:11 +0900

    feat: Add default og image

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -24,6 +24,7 @@ const Seo = ({ description, title, children }: SEOProps) => {
                         title
                         description
                         author
+                        ogImageUrl
                     }
                 }
             }
@@ -39,6 +40,7 @@ const Seo = ({ description, title, children }: SEOProps) => {
             <meta property="og:title" content={title} />
             <meta property="og:description" content={metaDescription} />
             <meta property="og:type" content="website" />
+            <meta property="og:image" content={site.siteMetadata.ogImageUrl} />
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:creator" content={site.siteMetadata.author} />
             <meta name="twitter:title" content={title} />
